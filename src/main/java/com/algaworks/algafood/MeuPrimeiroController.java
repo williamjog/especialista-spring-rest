63f45359e8131fd81b5811f67c048c7e2d6ba5a5
William Gomes <williamjog@hotmail.com>
2025-05-04 14:46:41 -0300

    feat: adiciona beans e entidade cliente | aula 2.11

--- src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/MeuPrimeiroController.java
@@ -1,10 +1,10 @@
 package com.algaworks.algafood;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MeuPrimeiroController {
 
     @GetMapping("/hello")
